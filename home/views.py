9ce6f4e9bb33ef936e2c2b6b05250a5d9ff9ee94
jpseixasesilva <jpseixasesilva@hotmail.com>
2019-07-25 22:50:17 -0300

    Template View

--- home/views.py
+++ home/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import logout
+from django.views.generic.base import TemplateView
 
 
 def home(request):
     #import pdb; pdb.set_trace()
-    c = {}
     a = 10
     b = 20
     c = a * b
@@ -14,3 +14,15 @@ def home(request):
 def my_logout(request):
     logout(request)
     return redirect('home')
+
+
+class HomePageView(TemplateView):
+    template_name = 'home2.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['my_variable'] = 'Hello, welcome!'
+        return context
+
+# FBV
+# CBV
