528878112771c2dc1cb71b659029be5921d0081b
jpseixasesilva <jpseixasesilva@hotmail.com>
2019-07-31 11:14:08 -0300

    Personal field forms 2

--- clientes/admin.py
+++ clientes/admin.py
@@ -4,9 +4,18 @@ from django.contrib.admin import ModelAdmin
 from .models import Person, Documento, Venda, Produto
 
 class PersonAdmin(admin.ModelAdmin):
-    fields = (('first_name', 'last_name', 'age'), 'salary', ('photo', 'bio'))
+    fieldsets = (
+        ('Dados Pessoais', {
+        'classes': ('collapse',),
+        'fields': ('first_name','last_name','doc')}),
+
+        ('Dados complementares', {
+        'classes': ('collapse',),
+        'fields': ('age','salary','photo')}))
+
+    #fields = (('first_name', 'last_name', 'age'), 'salary', ('photo', 'bio'))
     #exclude = ('bio',)
-    list_display = ('first_name', 'doc', 'last_name', 'age', 'salary', 'photo')
+    list_display = ('first_name', 'doc', 'last_name', 'age', 'salary', 'bio', 'photo')
 
 
 admin.site.register(Person, PersonAdmin)
