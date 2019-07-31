cc551ee1597e269757f3ee0f4a3fd0eeaa066ba6
jpseixasesilva <jpseixasesilva@hotmail.com>
2019-07-31 11:26:47 -0300

    Personal filters

--- clientes/admin.py
+++ clientes/admin.py
@@ -15,10 +15,14 @@ class PersonAdmin(admin.ModelAdmin):
 
     #fields = (('first_name', 'last_name', 'age'), 'salary', ('photo', 'bio'))
     #exclude = ('bio',)
-    list_display = ('first_name', 'doc', 'last_name', 'age', 'salary', 'bio', 'photo')
+    #list_display = ('first_name', 'doc', 'last_name', 'age', 'salary', 'bio', 'photo')
+    list_filter = ('age', 'salary',)
+
+class VendaAdmin(admin.ModelAdmin):
+    list_filter = ('pessoa__doc', 'desconto')
 
 
 admin.site.register(Person, PersonAdmin)
 admin.site.register(Documento)
-admin.site.register(Venda)
+admin.site.register(Venda, VendaAdmin)
 admin.site.register(Produto)
