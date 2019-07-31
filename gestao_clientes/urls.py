a3dbcf8c9c83a485f5cce8ad96b4238fbf2a4ddd
jpseixasesilva <jpseixasesilva@hotmail.com>
2019-07-31 15:19:06 -0300

    Headers, Index and Title

--- gestao_clientes/urls.py
+++ gestao_clientes/urls.py
@@ -33,3 +33,7 @@ if settings.DEBUG:
     urlpatterns = [
         path('__debug__/', include(debug_toolbar.urls)),
     ] + urlpatterns
+
+admin.site.site_header = 'Gestão de Clientes'
+admin.site.index_title = 'Administração'
+admin.site.site_title = 'Seja Bem Vindo ao Gestão de Clientes'
